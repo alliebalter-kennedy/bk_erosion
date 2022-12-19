50211441935d206513ce0f94a6db9e3c7b2f3b9d
Allie Balter-Kennedy <62767870+alliebalter-kennedy@users.noreply.github.com>
2022-12-19 11:22:17 -0500

    fixed issue with if statement

--- PofZ.m
+++ PofZ.m
@@ -12,6 +12,7 @@ function out = PofZ(zin,m,p,nuclide)
 %
 % 
 % Greg Balco -- Berkeley Geochronology Center -- May 2016
+% Updated by Allie Balter-Kennedy in Dec 2022 to include C-14
 
 
  
@@ -21,15 +22,15 @@ z = reshape(zin,1,numel(zin));
 inbounds = (z <= max(max(m.zz)));
 outofbounds = ~inbounds;
 
-if nuclide == 10;
+if nuclide == 10
     P10mu(inbounds) = interp1(m.zz,m.P10mu,z(inbounds));
     P10mu(outofbounds) = m.P10mu(end).*exp(-(z(outofbounds)-max(m.zz))./m.L10bot);
     out = P10mu + p.P10sp.*exp(-z./p.Lsp);
-else
+elseif nuclide ==26
     P26mu(inbounds) = interp1(m.zz,m.P26mu,z(inbounds));
     P26mu(outofbounds) = m.P26mu(end).*exp(-(z(outofbounds)-max(m.zz))./m.L26bot);
     out = P26mu + p.P26sp.*exp(-z./p.Lsp);
-elseif nuclide == 14;
+elseif nuclide == 14
     P14mu(inbounds) = interp1(m.zz,m.P14mu,z(inbounds));
     P14mu(outofbounds) = m.P14mu(end).*exp(-(z(outofbounds)-max(m.zz))./m.L14bot);
     out = P14mu + p.P14sp.*exp(-z./p.Lsp);
